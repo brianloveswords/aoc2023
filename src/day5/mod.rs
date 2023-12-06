edf87132bd5e0b320ec30dde3fa38893f7963aa8
Brian J Brennan <brianloveswords@gmail.com>
2023-12-05 20:55:35 -0500

    day 5: processing seeds

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -4,52 +4,67 @@ use crate::util::Range;
 
 pub const EXAMPLE: &str = include_str!("../../inputs/examples/day5.txt");
 
-pub fn part1(s: &str) -> usize {
-    let blocks: Vec<_> = s.split("\n\n").collect();
-
-    let seeds = blocks[0]
-        .split_once(':')
-        .expect("no seeds")
-        .1
-        .trim()
-        .split_whitespace()
-        .map(|s| s.parse::<usize>().expect("not a number"))
-        .collect::<Vec<_>>();
-
-    let maps: Vec<_> = blocks[1..]
-        .iter()
-        .map(|block| {
-            let result: Vec<_> = block
-                .split_once(':')
-                .expect("no seeds")
-                .1
-                .trim()
-                .lines()
-                .map(|line| {
-                    let numbers = line
-                        .split_whitespace()
-                        .map(|s| s.parse::<u32>().expect("not a number"))
-                        .collect::<Vec<_>>();
-                    assert!(numbers.len() == 3, "expected exactly 3 numbers");
-                    Conversion::new(numbers[0], numbers[1], numbers[2])
-                })
-                .collect();
-            Map::new(result)
-        })
-        .collect();
-
-    let almanac = Almanac { seeds, maps };
-
+pub fn part1(s: &str) -> u32 {
+    let almanac = Almanac::parse(s);
     println!("{almanac:?}");
-    println!("{blocks:?}");
-
-    0
+    almanac.process_seed(14)
 }
 
 #[derive(Debug)]
 struct Almanac {
-    seeds: Vec<usize>,
+    seeds: Vec<u32>,
     maps: Vec<Map>,
+}
+
+impl Almanac {
+    fn process_all_seeds(&self) -> Vec<u32> {
+        self.seeds.iter().map(|s| self.process_seed(*s)).collect()
+    }
+
+    fn process_seed(&self, seed: u32) -> u32 {
+        let mut result = seed;
+        for map in &self.maps {
+            result = map.apply(result) as u32;
+        }
+        result
+    }
+
+    fn parse(s: &str) -> Self {
+        let blocks: Vec<_> = s.split("\n\n").collect();
+
+        let seeds = blocks[0]
+            .split_once(':')
+            .expect("no seeds")
+            .1
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse::<u32>().expect("not a number"))
+            .collect::<Vec<_>>();
+
+        let maps: Vec<_> = blocks[1..]
+            .iter()
+            .map(|block| {
+                let result: Vec<_> = block
+                    .split_once(':')
+                    .expect("no seeds")
+                    .1
+                    .trim()
+                    .lines()
+                    .map(|line| {
+                        let numbers = line
+                            .split_whitespace()
+                            .map(|s| s.parse::<u32>().expect("not a number"))
+                            .collect::<Vec<_>>();
+                        assert!(numbers.len() == 3, "expected exactly 3 numbers");
+                        Conversion::new(numbers[0], numbers[1], numbers[2])
+                    })
+                    .collect();
+                Map::new(result)
+            })
+            .collect();
+
+        Almanac { seeds, maps }
+    }
 }
 
 #[derive(Debug)]
@@ -61,12 +76,15 @@ impl Map {
     fn new(conversions: Vec<Conversion>) -> Self {
         Self { conversions }
     }
-}
 
-#[derive(Debug)]
-enum Sign {
-    Positive,
-    Negative,
+    fn apply(&self, input: u32) -> u32 {
+        for conversion in &self.conversions {
+            if let Some(result) = conversion.attempt(input) {
+                return result;
+            }
+        }
+        input
+    }
 }
 
 struct Conversion {
@@ -118,6 +136,13 @@ impl Conversion {
         }
         input
     }
+
+    fn attempt(&self, input: u32) -> Option<u32> {
+        if self.matches(input) {
+            return Some((self.apply)(input));
+        }
+        None
+    }
 }
 
 #[cfg(test)]
@@ -125,7 +150,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn conversion_run() {
+    fn almanac_process_seed() {
+        let almanac = Almanac::parse(EXAMPLE);
+        assert_eq!(almanac.process_seed(79), 82);
+        assert_eq!(almanac.process_seed(14), 43);
+        assert_eq!(almanac.process_seed(55), 86);
+        assert_eq!(almanac.process_seed(13), 35);
+    }
+
+    #[test]
+    fn conversion_apply() {
         let conversion = Conversion::new(20, 30, 5);
         assert_eq!(conversion.apply(29), 29);
         assert_eq!(conversion.apply(30), 20);
@@ -134,5 +168,25 @@ mod tests {
         assert_eq!(conversion.apply(33), 23);
         assert_eq!(conversion.apply(34), 24);
         assert_eq!(conversion.apply(35), 35);
+    }
+
+    #[test]
+    fn map_apply() {
+        let map = Map::new(vec![
+            Conversion::new(30, 20, 1),
+            Conversion::new(32, 22, 2),
+            Conversion::new(35, 25, 3),
+        ]);
+
+        assert_eq!(map.apply(19), 19);
+        assert_eq!(map.apply(20), 30);
+        assert_eq!(map.apply(21), 21);
+        assert_eq!(map.apply(22), 32);
+        assert_eq!(map.apply(23), 33);
+        assert_eq!(map.apply(24), 24);
+        assert_eq!(map.apply(25), 35);
+        assert_eq!(map.apply(26), 36);
+        assert_eq!(map.apply(27), 37);
+        assert_eq!(map.apply(28), 28);
     }
 }
